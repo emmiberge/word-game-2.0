7338f86c22f23828ed1d8a741edf903ffac058a0
emmiberge <emmieberger@sogeti.se>
2024-05-02 11:14:06 +0200

    changed colors of tiles

--- src/model/Group.ts
+++ src/model/Group.ts
@@ -10,9 +10,9 @@ export enum Group{
 export class GroupClass{
     public static readonly groupColorMap : Map<Group, string> = 
     new Map([
-        [Group.PINK, "magenta"],
-        [Group.ORANGE, "orange"],
-        [Group.BLUE, "blue"],
-        [Group.YELLOW, "yellow"]
+        [Group.PINK, "rgb(197, 162, 255)"],
+        [Group.ORANGE, "rgb(246, 191, 74)"],
+        [Group.BLUE, "rgb(113, 187, 240)"],
+        [Group.YELLOW, "rgb(243, 225, 88)"]
     ]);
 }
